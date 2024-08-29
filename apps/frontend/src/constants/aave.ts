66281a63acc287c6cff30c9d6812ea5189c8ac90
matzapata <matuzapata@gmail.com>
2024-08-29 22:53:13 +0300

    remove whitelist

--- apps/frontend/src/constants/aave.ts
+++ apps/frontend/src/constants/aave.ts
@@ -10,7 +10,6 @@ export const config = {
   WETHGatewayAddress: '0x9F9Dc8dc9308C32Cf652b32dD1b5d2DA106f1dA0',
   UiPoolDataProviderV3Address: '0x0954E027fb7d59271CF253831c5Cc78be5c75840',
   PoolAddressesProviderAddress: '0x01757F5712695f8f93AD63ED3Bdf66575522188a',
-  assetsWhitelist: ['DAI', 'USDC', 'USDT', 'BTC', 'WETH', 'EURS'], // TODO: review this
 
   provider: new ethers.providers.JsonRpcProvider(
     'https://testnet.rpc.gobob.xyz',
