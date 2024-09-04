255b7a0a79c70ea10c9681c7376bf484274836b0
Matias Zapata <42716817+matzapata@users.noreply.github.com>
2024-09-04 19:22:30 +0300

    Connect topbar, add token links and add to wallet (#37)
    * connect topbar
    
    * tokens links
    
    * add link to oracle

--- apps/frontend/src/app/5_pages/AaveReserveOverviewPage/components/TopPanel/TopPanel.tsx
+++ apps/frontend/src/app/5_pages/AaveReserveOverviewPage/components/TopPanel/TopPanel.tsx
@@ -1,30 +1,89 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 
 import classNames from 'classnames';
 import { t } from 'i18next';
 
-import { Heading, Icon, IconNames, Paragraph, ParagraphSize } from '@sovryn/ui';
+import {
+  Heading,
+  Icon,
+  IconNames,
+  Paragraph,
+  ParagraphSize,
+  Tooltip,
+  TooltipPlacement,
+  TooltipTrigger,
+} from '@sovryn/ui';
+import { Decimal } from '@sovryn/utils';
+
+import { BOB_CHAIN_ID } from '../../../../../config/chains';
 
 import { WalletIcon } from '../../../../1_atoms/Icons/Icons';
 import { AmountRenderer } from '../../../../2_molecules/AmountRenderer/AmountRenderer';
 import { AssetRenderer } from '../../../../2_molecules/AssetRenderer/AssetRenderer';
 import { StatisticsCard } from '../../../../2_molecules/StatisticsCard/StatisticsCard';
-import { Reserve } from '../../../../../hooks/aave/useAaveReservesData';
+import { useNotifyError } from '../../../../../hooks/useNotifyError';
 import { translations } from '../../../../../locales/i18n';
+import { getBobExplorerUrl } from '../../../../../utils/helpers';
+import { formatUsdAmount } from './TopPanel.utils';
+import { ReserveTokens } from './components/ReserveTokens/ReserveTokens';
 
 const pageTranslations = translations.aaveReserveOverviewPage.topPanel;
 
-type CompactReserve = Pick<
-  Reserve,
-  'symbol' | 'name' | 'availableLiquidity' | 'liquidityRate' | 'priceOracle'
->;
+export type ReserveOverview = {
+  symbol: string;
+  name: string;
+  underlyingAsset: string;
+  aTokenAddress: string;
+  variableDebtTokenAddress: string;
+  stableDebtTokenAddress: string;
+  reserveSize: Decimal;
+  availableLiquidity: Decimal;
+  utilizationRate: Decimal;
+  oraclePrice: Decimal;
+  oracleAddress: string;
+};
 
 type TopPanelProps = {
-  reserve: CompactReserve | null;
+  reserve: ReserveOverview;
   className?: string;
 };
 
 export const TopPanel: FC<TopPanelProps> = ({ reserve, className }) => {
+  const { notifyError } = useNotifyError();
+
+  const openInExplorer = useCallback((tokenAddress: string) => {
+    const explorer = getBobExplorerUrl();
+    window.open(`${explorer}/address/${tokenAddress}`, '_blank');
+  }, []);
+
+  const addToWallet = useCallback(
+    (token: string) => {
+      try {
+        if (!(window as any)?.ethereum) {
+          throw new Error('Wallet not available');
+        }
+
+        (window as any)?.ethereum.request({
+          method: 'wallet_watchAsset',
+          params: {
+            type: 'ERC20',
+            options: {
+              chainId: BOB_CHAIN_ID,
+              address: token,
+            },
+          },
+        });
+      } catch (error) {
+        notifyError(error);
+      }
+    },
+    [notifyError],
+  );
+
+  const oracleLink = useMemo(() => {
+    return getBobExplorerUrl() + '/address/' + reserve.oracleAddress;
+  }, [reserve.oracleAddress]);
+
   return (
     <div className={classNames('w-full flex flex-col gap-6', className)}>
       <div className="text-center py-6 px-10 space-y-3 lg:hidden">
@@ -36,31 +95,73 @@ export const TopPanel: FC<TopPanelProps> = ({ reserve, className }) => {
         </Paragraph>
       </div>
 
-      <div className="gap-6 lg:gap-9 flex-shrink-0 grid grid-cols-2 lg:flex max-w-3xl">
+      <div className="gap-6 lg:gap-9 flex-shrink-0 grid grid-cols-2 lg:flex">
         <div className="col-span-2 flex items-center lg:items-start gap-3">
           <div className="flex items-center gap-1">
             <AssetRenderer
-              asset={reserve?.symbol || ''}
+              asset={reserve.symbol}
+              chainId={BOB_CHAIN_ID}
               showAssetLogo
               assetClassName="text-base"
               logoClassName="[&>svg]:h-8 [&>svg]:w-8 [&>svg]:mr-[10px]"
             />
             <span className="text-gray-40 text-base font-medium">
-              {reserve?.name || ''}
+              {reserve.name}
             </span>
           </div>
 
-          <div className="flex items-center gap-2 h-8">
-            <a href="#block-explorer">
-              <Icon
-                icon={IconNames.NEW_TAB}
-                className="text-gray-30"
-                size={16}
-              />
-            </a>
-            <a href="#block-explorer">
-              <Icon icon={WalletIcon} className="text-gray-30" size={16} />
-            </a>
+          <div className="flex items-center gap-3 h-8">
+            {/* show token in explorer */}
+            <Tooltip
+              tooltipClassName="py-1 px-0"
+              trigger={TooltipTrigger.hover}
+              placement={TooltipPlacement.bottom}
+              content={
+                <ReserveTokens
+                  className="w-60"
+                  symbol={reserve.symbol}
+                  aTokenAddress={reserve.aTokenAddress}
+                  underlyingTokenAddress={reserve.underlyingAsset}
+                  variableDebtTokenAddress={reserve.variableDebtTokenAddress}
+                  stableDebtTokenAddress={reserve.stableDebtTokenAddress}
+                  onClick={openInExplorer}
+                />
+              }
+            >
+              <div>
+                <Icon
+                  icon={IconNames.NEW_TAB}
+                  className="text-gray-30 cursor-pointer"
+                  size={16}
+                />
+              </div>
+            </Tooltip>
+
+            {/* add token to wallet */}
+            <Tooltip
+              tooltipClassName="py-1 px-0"
+              trigger={TooltipTrigger.hover}
+              placement={TooltipPlacement.bottom}
+              content={
+                <ReserveTokens
+                  className="w-60"
+                  symbol={reserve.symbol}
+                  aTokenAddress={reserve.aTokenAddress}
+                  underlyingTokenAddress={reserve.underlyingAsset}
+                  variableDebtTokenAddress={reserve.variableDebtTokenAddress}
+                  stableDebtTokenAddress={reserve.stableDebtTokenAddress}
+                  onClick={addToWallet}
+                />
+              }
+            >
+              <div>
+                <Icon
+                  icon={WalletIcon}
+                  className="text-gray-30 cursor-pointer"
+                  size={16}
+                />
+              </div>
+            </Tooltip>
           </div>
         </div>
 
@@ -69,9 +170,8 @@ export const TopPanel: FC<TopPanelProps> = ({ reserve, className }) => {
           value={
             <AmountRenderer
               prefix="$"
-              suffix="M"
-              value={'1234.56'} //@todo map this value
               className="text-2xl"
+              {...formatUsdAmount(reserve.reserveSize)}
             />
           }
         />
@@ -80,9 +180,8 @@ export const TopPanel: FC<TopPanelProps> = ({ reserve, className }) => {
           value={
             <AmountRenderer
               prefix="$"
-              suffix="M"
-              value={reserve?.availableLiquidity ?? '0'} //@todo ask this value
               className="text-2xl"
+              {...formatUsdAmount(reserve.availableLiquidity)}
             />
           }
         />
@@ -91,18 +190,20 @@ export const TopPanel: FC<TopPanelProps> = ({ reserve, className }) => {
           value={
             <AmountRenderer
               suffix="%"
-              value={reserve?.liquidityRate || 0} //@todo ask this value
+              value={reserve.utilizationRate}
+              precision={2}
               className="text-2xl"
             />
           }
         />
         <StatisticsCard
           label={t(pageTranslations.oraclePrice)}
-          link="#oracle-price"
+          link={oracleLink}
           value={
             <AmountRenderer
               prefix="$"
-              value={reserve?.priceOracle || 0} //@todo ask this value
+              value={reserve.oraclePrice}
+              precision={2}
               className="text-2xl"
             />
           }
