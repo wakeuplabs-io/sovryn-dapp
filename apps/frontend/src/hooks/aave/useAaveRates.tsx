2a3d6c35af93a4f796cb5204f707e003abae94aa
Luciano Perez Cerra <luchopcerra@gmail.com>
2024-09-11 14:17:27 -0300

    [SAF-20] improve calculateUtilizationRate

--- apps/frontend/src/hooks/aave/useAaveRates.tsx
+++ apps/frontend/src/hooks/aave/useAaveRates.tsx
@@ -20,19 +20,43 @@ export interface RatesDataResult {
   decimals: number;
 }
 
+export const isBetweenZeroAndOne = (value: Decimal): boolean => {
+  const lowerBound = Decimal.from(0);
+  const upperBound = Decimal.from(1);
+
+  // Check if value is greater than or equal to 0 and less than or equal to 1
+  return value.gte(lowerBound) && value.lte(upperBound);
+};
+
 export const calculateUtilizationRate = (
   decimals: number,
   totalDebt: string,
   availableLiquidity: string,
 ): Decimal => {
-  // Create BigInt instances
-  const totalBorrow = BigInt(utils.parseUnits(totalDebt, decimals).toString());
-  const totalSupply = BigInt(availableLiquidity) + totalBorrow;
-  // Perform division
-  const resultInBigInt = (totalBorrow * BigInt(10 ** decimals)) / totalSupply;
-  // Restore the decimal point
-  const finalResult = formatUnits(resultInBigInt.toString(), decimals);
-  return Decimal.from(finalResult);
+  const totalBorrowBigInt = BigInt(
+    utils.parseUnits(totalDebt, decimals).toString(),
+  );
+  const availableLiquidityBigInt = BigInt(availableLiquidity);
+
+  // Calculate total supply
+  const totalSupplyBigInt = totalBorrowBigInt + availableLiquidityBigInt;
+
+  // Ensure no division by zero
+  if (totalSupplyBigInt === BigInt(0)) {
+    return Decimal.from(0); // Return 0 if there's no total supply
+  }
+
+  // Perform the utilization rate calculation
+  const utilizationRateBigInt =
+    (totalBorrowBigInt * BigInt(10 ** decimals)) / totalSupplyBigInt;
+
+  // Convert the result back to a decimal string and then to a Decimal instance
+  const utilizationRate = utils.formatUnits(
+    utilizationRateBigInt.toString(),
+    decimals,
+  );
+
+  return Decimal.from(utilizationRate);
 };
 
 export const useAaveInterestRatesData = (
@@ -60,6 +84,9 @@ export const useAaveInterestRatesData = (
         reserve.totalDebt,
         reserve.availableLiquidity,
       );
+      if (!isBetweenZeroAndOne(utilizationRate)) {
+        console.log('The utilization rate is out of bounds.');
+      }
       setData({
         currentUsageRatio: utilizationRate,
         optimalUsageRatio: Decimal.from(
